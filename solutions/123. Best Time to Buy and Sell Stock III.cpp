05444223fa37de53696fe926f6373872807fa15c
Mohit Arora <cs.mohitaroraofficial@gmail.com>
2021-01-14 21:03:53 +0530

    132 ms

--- solutions/123. Best Time to Buy and Sell Stock III.cpp
+++ solutions/123. Best Time to Buy and Sell Stock III.cpp
@@ -5,19 +5,19 @@ public:
         int dp[size];
         for(int i=0;i<size;i++) dp[i]=0;
         
-        int selling = prices[size-1];
-        int purchasing = prices[0];
+        int sell_at = prices[size-1]; //Maximum
+        int purchase_at = prices[0];  //Minimum
         
         //Go from last to 1st element and add max profit in dp
         for(int i=size-2;i>=0;i--){
-            if(prices[i]>selling) selling=prices[i];
-            dp[i] = max(dp[i+1], selling-prices[i]);
+            if(prices[i]>sell_at) sell_at=prices[i];
+            dp[i] = max(dp[i+1], sell_at-prices[i]);
         }
         
         //Go from 1st to last element and add max profit in previous dp
         for(int i=1;i<size;i++){
-            if(prices[i]<purchasing) purchasing=prices[i];
-            dp[i] = max(dp[i-1],dp[i]+(prices[i]-purchasing));
+            if(prices[i]<purchase_at) purchase_at=prices[i];
+            dp[i] = max(dp[i-1],dp[i]+(prices[i]-purchase_at));
         }
         
         return dp[size-1];
