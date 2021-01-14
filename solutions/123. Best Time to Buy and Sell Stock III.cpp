85dcc75150e25f26591f77100b5563ac158652f6
Mohit Arora <cs.mohitaroraofficial@gmail.com>
2021-01-14 21:33:51 +0530

    120 ms

--- solutions/123. Best Time to Buy and Sell Stock III.cpp
+++ solutions/123. Best Time to Buy and Sell Stock III.cpp
@@ -1,3 +1,23 @@
+class Solution {
+public:
+    int maxProfit(vector<int>& prices) {
+​
+        //DP APPROACH
+        
+        int size = prices.size();
+        int dp[size];
+        for(int i=0;i<size;i++) dp[i]=0;
+        
+        int sell_at = prices[size-1]; //Maximum
+        int purchase_at = prices[0];  //Minimum
+        
+        //Go from last to 1st element and add max profit in dp
+        for(int i=size-2;i>=0;i--){
+            if(prices[i]>sell_at) sell_at=prices[i];
+            dp[i] = max(dp[i+1], sell_at-prices[i]);
+        }
+        
+        //Go from 1st to last element and add max profit in previous dp
         for(int i=1;i<size;i++){
             if(prices[i]<purchase_at) purchase_at=prices[i];
             dp[i] = max(dp[i-1],dp[i]+(prices[i]-purchase_at));
