0097c58dd605c5bbb943b90bbbb5dd9a927559dc
Mohit Arora <cs.mohitaroraofficial@gmail.com>
2026-01-10 02:37:58 +0530

    Time: 0 ms (100%), Space: 16.1 MB (79.92%) - LeetHub

--- 0865-smallest-subtree-with-all-the-deepest-nodes/0865-smallest-subtree-with-all-the-deepest-nodes.cpp
+++ 0865-smallest-subtree-with-all-the-deepest-nodes/0865-smallest-subtree-with-all-the-deepest-nodes.cpp
@@ -10,31 +10,59 @@
  * right(right) {}
  * };
  */
+
+// Two-Pass DFS Approach -> Finding the maxDepth of Tree -> Finding max Depth
+// Parent
+//  class Solution {
+//  public:
+//      TreeNode* subtreeWithAllDeepest(TreeNode* root) {
+//          int d = maxDepth(root);
+//          return find(root, 1, d);
+//      }
+
+// private:
+//     int maxDepth(TreeNode* root) {
+//         if (!root)
+//             return 0;
+//         return 1 + max(maxDepth(root->left), maxDepth(root->right));
+//     }
+
+//     TreeNode* find(TreeNode* root, int depth, int maxD) {
+//         if (!root) return nullptr;
+//         if (depth == maxD) return root;
+
+//         TreeNode* left = find(root->left, depth + 1, maxD);
+//         TreeNode* right = find(root->right, depth + 1, maxD);
+
+//         if (left && right) return root;
+//         return left ? left : right;
+//     }
+// };
+
+// Single Pass DFS Approach: Finding max depth and root along with it
+// Bottom-Up approach
 class Solution {
 public:
-    TreeNode* subtreeWithAllDeepest(TreeNode* root) {
-        int d = maxDepth(root);
-        return find(root, 1, d);
+    TreeNode* subtreeWithAllDeepest(TreeNode* root) { 
+        return dfs(root).second;
     }
 
 private:
-    int maxDepth(TreeNode* root) {
-        if (!root)
-            return 0;
-        return 1 + max(maxDepth(root->left), maxDepth(root->right));
-    }
+    pair<int, TreeNode*> dfs(TreeNode* root) {
+        if (!root) return {0, nullptr}; // depth 0 for empty tree (bottoms up approach: start from bottom with 0 depth of the left most child's child which doesn't exist)
 
-    TreeNode* find(TreeNode* root, int depth, int maxD) {
-        if (!root)
-            return nullptr;
-        if (depth == maxD)
-            return root;
+        auto left = dfs(root->left);
+        auto right = dfs(root->right);
 
-        TreeNode* left = find(root->left, depth + 1, maxD);
-        TreeNode* right = find(root->right, depth + 1, maxD);
+        // Now left.first and right.first are ALREADY known depths
+        // from the child subtrees
 
-        if (left && right)
-            return root;
-        return left ? left : right;
+        if (left.first > right.first) // left subtree is deeper
+            return {left.first + 1, left.second};
+
+        if (right.first > left.first) // right subtree is deeper
+            return {right.first + 1, right.second};
+
+        return {left.first + 1, root}; // both sides have equal depth (first step depth start here on both sides of the tree while traversing)
     }
 };
